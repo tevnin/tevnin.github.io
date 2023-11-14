e4a90598d605eb1769499d7b2982e54302d493ab
Tami Evnin <tami.evnin@gmail.com>
2023-11-14 15:01:10 -0600

    setting up the basics
    - index & portfolio index pages
    - copied over main content

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add your JavaScript code here
+
+//MAIN HEADER & NAV
+// fetch('header.html')
+// .then(response => response.text())
+// .then(html => {
+//   document.getElementById('header-placeholder').innerHTML = html;
+// });
